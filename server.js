f34730e467e4578c9a6476820d500f3a7a427342
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-02-23 10:03:17 -0800

    Implemented DB connection

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const actionitems = require('./routes/actionitems');
+const connectDB = require('./config/db');
 
 // Load enviroment variables
 dotenv.config();
+
+// Connect to database
+connectDB();
+
+// Route files
+const actionitems = require('./routes/actionitems');
 
 const app = express();
 
@@ -20,7 +24,14 @@ app.use('/api/v1/actionitems', actionitems);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
